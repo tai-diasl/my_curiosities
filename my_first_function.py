838d2d98b9bc2f51278c7a59247a4bf45061b4fc
Taís Dias <taisayana@hotmail.com>
2023-02-15 13:09:38 +0000

    This is my first Python function. I had no idea how to use it. It is extremely simple, I know, but still I am very happy!

---
+++ my_first_function.py
@@ -0,0 +1,10 @@
+def my_first_function():
+    print("Hello World! This is my first function in Python!")
+    print("I am really happy! =)")
+
+
+# It is necessary to leave 2 blank lines after finishing the function
+
+print("Below I will call the function\n")
+my_first_function()
+print("\nFinishing!")
